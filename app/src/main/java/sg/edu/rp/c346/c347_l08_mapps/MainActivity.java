ecfef20f10126fa1aa3a2d50d1c5bf7a4d5b2e94
Bryan Poh <zandrex00@gmail.com>
2020-07-13 22:24:27 +0800

    Advanced Enhancement

--- app/src/main/java/sg/edu/rp/c346/c347_l08_mapps/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/c347_l08_mapps/MainActivity.java
@@ -10,7 +10,9 @@ import android.Manifest;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdate;
@@ -18,6 +20,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
@@ -27,7 +30,8 @@ public class MainActivity extends AppCompatActivity
 {
   Button btn1, btn2, btn3;
   private GoogleMap map;
-  LatLng poi_hq, poi_central, poi_east;
+  LatLng poi_singapore,poi_hq, poi_central, poi_east;
+  Spinner spinner;
 
   @Override
   protected void onCreate(Bundle savedInstanceState)
@@ -35,13 +39,38 @@ public class MainActivity extends AppCompatActivity
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
+    poi_singapore = new LatLng(1.3521, 103.8198);
     poi_hq = new LatLng(1.424450, 103.829849);
     poi_central = new LatLng(51.592770, -0.195460);
     poi_east = new LatLng(53.595428, -1.308810);
+    spinner = findViewById(R.id.spinner);
+
+    // Set spinner
+    spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+      @Override
+      public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        if(map != null){
+          switch (i){
+            case 0:
+              map.moveCamera(CameraUpdateFactory.newLatLngZoom(poi_hq, 16));
+            case 1:
+              map.moveCamera(CameraUpdateFactory.newLatLngZoom(poi_central, 16));
+            case 2:
+              map.moveCamera(CameraUpdateFactory.newLatLngZoom(poi_east, 16));
+            default:
+              map.moveCamera(CameraUpdateFactory.newLatLngZoom(poi_hq, 16));
+          }
+        }
+      }
+
+      @Override
+      public void onNothingSelected(AdapterView<?> adapterView) {
+
+      }
+    });
 
     FragmentManager fm = getSupportFragmentManager();
-    SupportMapFragment mapFragment = (SupportMapFragment)
-            fm.findFragmentById(R.id.map);
+    SupportMapFragment mapFragment = (SupportMapFragment) fm.findFragmentById(R.id.map);
 
     mapFragment.getMapAsync(new OnMapReadyCallback()
     {
@@ -50,8 +79,8 @@ public class MainActivity extends AppCompatActivity
       {
         map = googleMap;
 
-        // Set default loc when start
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(poi_hq, 11));
+        // Set default loc to Singapore
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(poi_singapore, 11));
 
         // Set toast on marker click to show toast
         map.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener()
@@ -64,6 +93,7 @@ public class MainActivity extends AppCompatActivity
           }
         });
 
+        // Check for permission granted in location Access
         int permissionCheck = ContextCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.ACCESS_FINE_LOCATION);
         if (permissionCheck == PermissionChecker.PERMISSION_GRANTED) {
           map.setMyLocationEnabled(true);
@@ -74,8 +104,9 @@ public class MainActivity extends AppCompatActivity
         }
 
         // Map tools
-        map.getUiSettings().setCompassEnabled(true);
-        map.getUiSettings().setZoomControlsEnabled(true);
+        UiSettings uiSettings = map.getUiSettings();
+        uiSettings.setZoomControlsEnabled(true);
+        uiSettings.setCompassEnabled(true);
 
         Marker hq = map.addMarker(new
                 MarkerOptions()
@@ -83,7 +114,7 @@ public class MainActivity extends AppCompatActivity
                 .title("HQ - North")
                 .snippet("Block 333, Admiralty Ave 3, 765654 Operating hours: 10am-5pm\n" +
                         "Tel:65433456")
-                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)));
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_launcher)));
 
         Marker central = map.addMarker(new
                 MarkerOptions()
@@ -92,7 +123,7 @@ public class MainActivity extends AppCompatActivity
                 .snippet("Block 3A, Orchard Ave 3, 134542 \n" +
                         "Operating hours: 11am-8pm\n" +
                         "Tel:67788652")
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_launcher)));
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)));
 
         Marker east = map.addMarker(new
                 MarkerOptions()
@@ -101,11 +132,11 @@ public class MainActivity extends AppCompatActivity
                 .snippet("Block 555, Tampines Ave 3, 287788 \n" +
                         "Operating hours: 9am-5pm\n" +
                         "Tel:66776677")
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_launcher)));
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
       }
     });
 
-    btn1 = findViewById(R.id.btn1);
+    /*btn1 = findViewById(R.id.btn1);
     btn2 = findViewById(R.id.btn2);
     btn3 = findViewById(R.id.btn3);
 
@@ -140,6 +171,6 @@ public class MainActivity extends AppCompatActivity
           map.moveCamera(CameraUpdateFactory.newLatLngZoom(poi_east, 11));
         }
       }
-    });
+    });*/
   }
 }
